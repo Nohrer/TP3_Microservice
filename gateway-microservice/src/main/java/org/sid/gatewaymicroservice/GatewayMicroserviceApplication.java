882805a884e4a47af2c308ad5207131d02badfeb
Nohrer <naoufelguendouz03@gmail.com>
2025-10-21 14:25:13 +0100

    ADD Dynamic Discovery Service

--- gateway-microservice/src/main/java/org/sid/gatewaymicroservice/GatewayMicroserviceApplication.java
+++ gateway-microservice/src/main/java/org/sid/gatewaymicroservice/GatewayMicroserviceApplication.java
@@ -1,7 +1,12 @@
 package org.sid.gatewaymicroservice;
 
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayMicroserviceApplication {
@@ -10,4 +15,11 @@ public class GatewayMicroserviceApplication {
         SpringApplication.run(GatewayMicroserviceApplication.class, args);
     }
 
+
+    //create dynamic routes
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+
+    }
 }
